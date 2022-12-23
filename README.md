f5f9ee5ef250eaaa595393c4a867ec45ec05738b
idreesalmasri <idrees.massri95@gmail.com>
2022-12-23 17:39:15 +0200

    save record section completed

---
+++ README.md
@@ -0,0 +1,14 @@
+# Covid-19-statistics-Front-End  
+
+- a web app used to view Covid-19-statistics, including cases with different types for countries.
+- you can find the back-end server here [Back-End repo](https://github.com/idreesalmasri/Covid-19-statistics-Back-End)
+
+## User Story:
+1. The user can view the overall total statistics around the world, such as confirmed cases, deaths cases, and recovered cases.
+2. The user can filter the statistics for a specific country and for a specific period of time.
+3. The user can view statistics in detail for all countries.
+4. The user can save any statistics record for any country in his database if needed.
+5. The user can view the saved records and delete any one of them.
+
+### Deploy link 
+[Covid-19-statistics-Front-End]()
--- src/Components/Main/Home/Total/Cards.js
+++ src/Components/Main/Home/Total/Cards.js
@@ -1,41 +1,41 @@
 
 
-const Cards=(props)=>{
-    
-    let statisticsKeys=Object.keys(props.data);
-    return(
+const Cards = (props) => {
+
+    let statisticsKeys = Object.keys(props.data);
+    return (
         <div style={{
-            display:"flex",
-            justifyContent:"space-between",
-            margin:'auto',
-            width:'700px',
-            }}>
+            display: "flex",
+            justifyContent: "space-between",
+            margin: 'auto',
+            width: '700px',
+        }}>
             {
-                statisticsKeys.map((e,index)=>{
-                    return(
-                        
-                        <div 
-                        className='card' 
-                        key={index}
-                        style={{
-                            width:'140px',
-                            height:"100px",
-                            boxShadow:"0 4px 8px 0 rgba(0,0,0,1)",
-                            transition:"0.3s",
-                            padding:'2px 14px',
-                            backgroundColor:"grey",
-                        }}
-                        >
-                            <h3 
+                statisticsKeys.map((e, index) => {
+                    return (
+
+                        <div
+                            className='card'
+                            key={index}
                             style={{
-                                color:"black",
-                                textAlign:"center",
+                                width: '140px',
+                                height: "100px",
+                                boxShadow: "0 4px 8px 0 rgba(0,0,0,1)",
+                                transition: "0.3s",
+                                padding: '2px 14px',
+                                backgroundColor: "#008b8b",
                             }}
+                        >
+                            <h3
+                                style={{
+                                    color: "black",
+                                    textAlign: "center",
+                                }}
                             > {e}</h3>
                             <hr></hr>
-                            <p style={{textAlign:"center"}}>{props.data[e]}</p>
+                            <p style={{ textAlign: "center" }}>{props.data[e]}</p>
                         </div>
-                        
+
                     )
                 })
             }
